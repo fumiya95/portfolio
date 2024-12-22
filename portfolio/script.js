dc455c1e7f1dd011fbeb450a13f73b74fedae65a
fumiya <156275318+fumiya95@users.noreply.github.com>
2024-12-22 22:07:27 +0900

    Add files via upload

--- portfolio/script.js
+++ portfolio/script.js
@@ -3,9 +3,5 @@ const hamburger = document.getElementById("hamburger");
 const navUl = document.querySelector(".header__nav ul");
 
 hamburger.addEventListener("click", () => {
-  if (navUl.style.display === "flex") {
-    navUl.style.display = "none";
-  } else {
-    navUl.style.display = "flex";
-  }
+  navUl.style.display = (navUl.style.display === "flex") ? "none" : "flex";
 });
